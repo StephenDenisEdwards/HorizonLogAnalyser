4bbd30f319e8388fe084923a1a0ed81242ca3b7b
stephen <stephen.denis.edwards@googlemail.com>
2020-11-19 23:31:13 +0100

    ReportType using C# 9 record type (NOTE: problem with the VS2019 16.8.2 syntax highlighting)

--- ConsoleW3CLogAnalyzer/ReportType.cs
+++ ConsoleW3CLogAnalyzer/ReportType.cs
@@ -1,22 +1,14 @@
 ﻿namespace ConsoleW3CLogAnalyzer
 {
-    public class ReportType
+    public record ReportType
     {
         public string Value { get; set; }
         public int Rank { get; set; }
         public int Hits { get; set; }
 
-        public override string ToString()
-        {
-            return $"Value:{Value}, Rank:{Rank}, Hits:{Hits}";
-        }
-    }
-
-    public record Person
-    {
-    public string LastName { get; }
-    public string FirstName { get; }
-
-    public Person(string first, string last) => (FirstName, LastName) = (first, last);
+        //public override string ToString()
+        //{
+        //    return $"Value:{Value}, Rank:{Rank}, Hits:{Hits}";
+        //}
     }
 }
