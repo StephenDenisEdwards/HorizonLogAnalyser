0b97047c179be53d721452e0eb9a77179f4332d5
stephen <stephen.denis.edwards@googlemail.com>
2020-11-19 23:08:00 +0100

    c#9

--- ConsoleW3CLogAnalyzer/ReportType.cs
+++ ConsoleW3CLogAnalyzer/ReportType.cs
@@ -11,4 +11,12 @@
             return $"Value:{Value}, Rank:{Rank}, Hits:{Hits}";
         }
     }
+
+    public record Person
+    {
+    public string LastName { get; }
+    public string FirstName { get; }
+
+    public Person(string first, string last) => (FirstName, LastName) = (first, last);
+    }
 }
